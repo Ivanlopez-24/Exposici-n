ec0a65094edb4fc1c6666fb2b8bed5e688c5164d
JUAN DIEGO MARTINEZ BELTRAN <juandiegomartinez813@gmail.com>
2025-10-05 12:55:53 -0500

    Prueba

--- Parcial/src/main/java/com/mycompany/parcial/Parcial.java
+++ Parcial/src/main/java/com/mycompany/parcial/Parcial.java
@@ -11,6 +11,6 @@ package com.mycompany.parcial;
 public class Parcial {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! hola");
+        System.out.println("Hello World! ");
     }
 }
