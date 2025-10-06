39b8b547914f1d581941f83db55214b12029bc68
JUAN DIEGO MARTINEZ BELTRAN <juandiegomartinez813@gmail.com>
2025-10-06 15:51:06 -0500

    Separo la implementación

---
+++ Parcial/src/main/java/Exporter.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jdieg
+ */
+public interface Exporter {
+    String export(String titulo, String cuerpo);
+}
